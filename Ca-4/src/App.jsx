f4979ef6c3f7603718944d0786947139a480160b
mugunthan s <mugunthan701@gmail.com>
2023-12-14 14:32:46 +0530

    comp

--- Ca-4/src/App.jsx
+++ Ca-4/src/App.jsx
@@ -1,30 +1,44 @@
-import { useState, useEffect } from 'react';
+/* eslint-disable no-unused-vars */
+import React, { useState } from 'react';
+import { BsBrightnessHighFill } from 'react-icons/bs';
+import Question from './Question';
 import logo from './assets/logo.png';
 import './App.css';
-import { BsBrightnessHigh } from 'react-icons/bs';
-import Question from './Question';
-
 
 function App() {
-  const colorChange = ()=>{
-    
-  }
+  const [isLightMode, setLightMode] = useState(true);
+
+  const toggleColor = () => {
+    setLightMode((prevMode) => !prevMode);
+  };
+
+  const themeColor = isLightMode ? 'black' : 'white';
+  const backgroundClr = isLightMode
+    ? 'linear-gradient(180deg, #B3F6FF 0%, #6FFFCB 100%)'
+    : 'rgba(16, 0, 0, 1)';
+
   return (
     <>
-      <nav id='nav-bar'>
-        <img id='logoimg' src={logo} alt='' />
-        <div id='theme'>
-          <BsBrightnessHigh size={"50px"} 
-           />
+      <div id='body' style={{ background: backgroundClr }}>
+        <nav id='nav-bar'>
+          <img id='logoimg' src={logo} alt='' />
+          <div id='theme'>
+            <BsBrightnessHighFill
+              onClick={toggleColor}
+              fill={themeColor}
+              size='50px'
+            />
+          </div>
+        </nav>
+        <div id='main1'>
+          <div id='main'>
+            <Question isLightmode={isLightMode} />
+          </div>
         </div>
-      </nav>
-<div id='main1'>
-      <div id='main'>
-        <Question></Question>
-      </div>
       </div>
     </>
   );
 }
 
 export default App;
+
--- Ca-4/src/Question.jsx
+++ Ca-4/src/Question.jsx
@@ -1,7 +1,8 @@
-import React,{useState} from 'react';
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+import React, { useState } from 'react';
 
-
-function Question() {
+function Question({ isLightmode }) {
   const questions = [
     {
       text: "What is ReactJS?",
@@ -13,46 +14,43 @@ function Question() {
       ],
     },
     {
-        text: "React.js is written in which of the following language?",
-        options: [
-          { id: 0, text: "JavaScript", isCorrect: true },
-          { id: 1, text: "Java", isCorrect: false },
-          { id: 2, text: "C", isCorrect: false },
-          { id: 3, text: "C++", isCorrect:  false },
-        ],
-      },
-      {
-        text: "What is a state in React?",
-        options: [
-          { id: 0, text: "A permanent storage.", isCorrect:  true },
-          { id: 1, text: "Internal storage of the component.", isCorrect:false },
-          { id: 2, text: "External storage of the component.", isCorrect: false },
-          { id: 3, text: "None of the above.", isCorrect: false },
-        ],
-      },
-      
-      {
-        text: "What is the return value of the useState hook?",
-        options: [
-          { id: 0, text: " Pair of current state and function updating it", isCorrect: true },
-          { id: 1, text: "Current State", isCorrect: false },
-          { id: 2, text: "Function updating the current state", isCorrect:  false },
-          { id: 3, text: "UseState returns nothing", isCorrect: false },
-        ],
-      },
-      {
-        text: "How many elements can a valid react component return?",
-        options: [
-          { id: 0, text: "1", isCorrect: true },
-          { id: 1, text: "2", isCorrect:  false },
-          { id: 2, text: "3", isCorrect:  false },
-          { id: 3, text: "4", isCorrect:  false },
-        ],
-      },
+      text: "React.js is written in which of the following languages?",
+      options: [
+        { id: 0, text: "JavaScript", isCorrect: true },
+        { id: 1, text: "Java", isCorrect: false },
+        { id: 2, text: "C", isCorrect: false },
+        { id: 3, text: "C++", isCorrect: false },
+      ],
+    },
+    {
+      text: "What is a state in React?",
+      options: [
+        { id: 0, text: "A permanent storage.", isCorrect: true },
+        { id: 1, text: "Internal storage of the component.", isCorrect: false },
+        { id: 2, text: "External storage of the component.", isCorrect: false },
+        { id: 3, text: "None of the above.", isCorrect: false },
+      ],
+    },
+    {
+      text: "What is the return value of the useState hook?",
+      options: [
+        { id: 0, text: "Pair of current state and function updating it", isCorrect: true },
+        { id: 1, text: "Current State", isCorrect: false },
+        { id: 2, text: "Function updating the current state", isCorrect: false },
+        { id: 3, text: "UseState returns nothing", isCorrect: false },
+      ],
+    },
+    {
+      text: "How many elements can a valid React component return?",
+      options: [
+        { id: 0, text: "1", isCorrect: true },
+        { id: 1, text: "2", isCorrect: false },
+        { id: 2, text: "3", isCorrect: false },
+        { id: 3, text: "4", isCorrect: false },
+      ],
+    },
   ];
 
-  
-  
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [correctAnswers, setCorrectAnswers] = useState(0);
   const [isHighlighted, setIsHighlighted] = useState(false);
@@ -68,7 +66,7 @@ function Question() {
   const handleReset = () => {
     setCurrentQuestionIndex(0);
     setCorrectAnswers(0);
-    setIsHighlighted(false); // Reset highlight state
+    setIsHighlighted(false);
   };
 
   const handleHighlight = () => {
@@ -95,22 +93,35 @@ function Question() {
             Question {currentQuestionIndex + 1} out of {questions.length}
           </p>
           <div id='prim-div'>
-            <div id='sec-div'>
-              <p id='question' style={{ color: isHighlighted ? 'red' : 'inherit' }}>
+            <div
+              id='sec-div'
+              style={{ backgroundColor: isLightmode ? 'rgba(221, 182, 151, 0.77)' : '#000' }}
+            >
+              <p
+                id='question'
+                style={{ color: isHighlighted ? 'red' : (isLightmode ? 'inherit' : 'white') }}
+              >
                 {questions[currentQuestionIndex].text}
               </p>
               <ul>
                 {questions[currentQuestionIndex].options.map((option) => (
                   <li key={option.id}>
-                    <button onClick={() => handleOptionClick(option.isCorrect)}>
+                    <button
+                      className={isLightmode ? 'light-button' : 'dark-button'}
+                      onClick={() => handleOptionClick(option.isCorrect)}
+                    >
                       {option.text}
                     </button>
                   </li>
                 ))}
               </ul>
               <div>
-                <button className='high' onClick={handleHighlight}>Highlight</button>
-                <button className='high' onClick={handleUnhighlight}>Unhighlight</button>
+                <button className='high' onClick={handleHighlight}>
+                  Highlight
+                </button>
+                <button className='high' onClick={handleUnhighlight}>
+                  Unhighlight
+                </button>
               </div>
             </div>
           </div>
